0cb01b8e145912af0160b4fecf013c0baf05dde9
D3r3k23 <d3r3k523@gmail.com>
2022-04-29 22:58:15 -0700

    Rm comments

--- project/EOS/USER/cmd/cat.c
+++ project/EOS/USER/cmd/cat.c
@@ -1,20 +1,9 @@
 #include "ucode.c"
 
-void write_console(const char* s)
-{
-    int fd = open("/dev/tty0", O_WRONLY);
-    if (fd != -1) {
-        write(fd, s, strlen(s));
-        close(fd);
-    }
-}
-
 int main(int argc, char* argv[])
 {
-    // write_console("CAT\n\r");
     int total = 0;
     if (argc >= 2) { // cat file
-        // write_console("cat file\n\r");
         const char* filename = argv[1];
 
         STAT st;
@@ -33,42 +22,33 @@ int main(int argc, char* argv[])
             } else {
                 char buf[2];
                 int n;
-                // write_console("START CATTING\n\r");
                 while (n = read(fd, buf, 1)) {
                     total += n;
                     if (isatty(STDOUT)) {
-                        // write_console("WRITE CONSOLE\n\r");
                         prints(buf);
                     } else {
-                        // write_console("WRITE:\n\r");
-                        // write_console(buf);
                         write(STDOUT, buf, n);
                     }
                     bzero(buf, 2);
                 }
-                // write_console("FINSIH CATTING\n\r");
                 close(fd);
             }
         }
     } else { // cat stdin
-        // write_console("cat stdin\n\r");
         char lc = '\0';
         char c;
         while (read(STDIN, &c, 1)) {
             total++;
             if (isatty(STDOUT)) {
-                // write_console("WRITE CONSOLE\n\r");
                 if (c == '\r') {
                     printc('\n');
                 }
                 printc(c);
             } else {
-                // write_console("WRITE\n\r");
                 write(STDOUT, &c, 1);
             }
         }
         lc = c;
     }
-    // write_console("CAT EXIT\n\r");
     return !total;
 }
